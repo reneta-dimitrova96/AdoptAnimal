a56d58dd57a2d58aef37d430710067e16bd1976a
Reneta Dimitrova <rd@nomadcy.com>
2022-03-13 21:57:59 +0200

    Created AdsController and CreateAdInputModel

--- Web/AdoptAnimal.Web.ViewModels/Ads/CreateAdInputModel.cs
+++ Web/AdoptAnimal.Web.ViewModels/Ads/CreateAdInputModel.cs
@@ -14,7 +14,7 @@
         public string Description { get; set; }
 
         public string AuthorId { get; set; }
-         
+
         public int PetId { get; set; }
     }
 }
---
+++ Web/AdoptAnimal.Web/Controllers/AdsController.cs
@@ -0,0 +1,37 @@
+﻿namespace AdoptAnimal.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using AdoptAnimal.Services.Data;
+    using AdoptAnimal.Web.ViewModels.Ads;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AdsController : Controller
+    {
+        private readonly IAdService adsService;
+
+        public AdsController(IAdService adsService)
+        {
+            this.adsService = adsService;
+        }
+
+        public IActionResult Create()
+        {
+            var viewModel = new CreateAdInputModel();
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateAdInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            await this.adsService.CreateAsync(input);
+
+            return this.Redirect("/");
+        }
+    }
+}
