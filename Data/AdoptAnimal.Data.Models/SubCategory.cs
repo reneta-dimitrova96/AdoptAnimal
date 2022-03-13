915c022d05a4d6ed9d91906e167bb442ace1ea2d
Reneta Dimitrova <rd@nomadcy.com>
2022-03-13 18:07:18 +0200

    Add new model - SubCategory

---
+++ Data/AdoptAnimal.Data.Models/SubCategory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoptAnimal.Data.Models
+{
+    internal class SubCategory
+    {
+    }
+}
---
+++ Data/AdoptAnimal.Data/Migrations/20220313160511_AddSubCategoryModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdoptAnimal.Data.Migrations
+{
+    public partial class AddSubCategoryModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
