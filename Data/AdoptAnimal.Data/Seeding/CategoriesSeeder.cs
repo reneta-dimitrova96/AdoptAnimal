06d30f5a9397542cc49141bb23243a32a705e1f2
Reneta Dimitrova <rd@nomadcy.com>
2022-03-13 18:34:23 +0200

    Added SubCategory in ApplicationDbContext, added CategoriesSeeder and SubCategoriesSeeder

---
+++ Data/AdoptAnimal.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,25 @@
+﻿namespace AdoptAnimal.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using AdoptAnimal.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            await dbContext.Categories.AddAsync(new Category { Name = "Кучета" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Котки" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Гризачи" });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Data/AdoptAnimal.Data/Seeding/SubCategoriesSeeder.cs
@@ -0,0 +1,36 @@
+﻿namespace AdoptAnimal.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using AdoptAnimal.Data.Models;
+
+    internal class SubCategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.SubCategories.Any())
+            {
+                return;
+            }
+
+            var arrayOfCategories = dbContext.Categories.Where(c => c.Name == "Гризачи").ToArray();
+            if (arrayOfCategories.Length > 0)
+            {
+                await dbContext.SubCategories.AddAsync(new SubCategory
+                {
+                    Name = "Зайци",
+                    CategoryId = arrayOfCategories[0].Id,
+                });
+                await dbContext.SubCategories.AddAsync(new SubCategory
+                {
+                    Name = "Хамстери",
+                    CategoryId = arrayOfCategories[0].Id,
+                });
+            }
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
